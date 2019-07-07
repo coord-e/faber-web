a544a7bfccded46438839114d3a8dc60233f0b3c
coord.e <me@coord-e.com>
2019-07-07 04:56:19 +0000

    Add: Make the layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import MonacoEditor from 'react-monaco-editor';
 
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button, Columns } from 'react-bulma-components';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       code: '// type your code...',
+      stdout: '',
+      stderr: '',
     }
     this.onChange = this.onChange.bind(this)
     this.editorDidMount = this.editorDidMount.bind(this)
   }
   editorDidMount(editor, monaco) {
-    console.log('editorDidMount', editor);
     editor.focus();
   }
   onChange(newValue, e) {
@@ -23,16 +27,27 @@ class App extends React.Component {
       selectOnLineNumbers: true
     };
     return (
+      <Columns>
+        <Columns.Column>
         <MonacoEditor
-      width="800"
-      height="600"
-      language="javascript"
-      theme="vs-dark"
-      value={code}
-      options={options}
-      onChange={this.onChange}
-      editorDidMount={this.editorDidMount}
+            width="100%"
+            height="100vh"
+            language="javascript"
+            theme="vs-dark"
+            value={code}
+            options={options}
+            onChange={this.onChange}
+            editorDidMount={this.editorDidMount}
         />
+        </Columns.Column>
+        <Columns.Column>
+            <div id="toolbox">
+                <Button>Run</Button>
+            </div>
+            <div id="stdout">{this.state.stdout}</div>
+            <div id="stderr">{this.state.stderr}</div>
+        </Columns.Column>
+    </Columns>
     );
   }
 }
